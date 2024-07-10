dc17aa79b8e769e3b6a3cb299e76b46be33838c5
anna <ani.kolesnikova@gmail.com>
2024-07-10 15:37:37 +0200

    fix: delete comments, add husky hook

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,19 +1,3 @@
-// import { ICharacter } from "../../types/types";
-// import "./CharacterCard.scss";
-
-// export default function CharacterCard({ name, image }: ICharacter) {
-//   return (
-//     <div className="character-card" data-testid="character-card">
-//       <h5 className="character-name">{name}</h5>
-//       <div className="character-container">
-//         <div className="character-img">
-//           <img src={image} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { Component } from "react";
 import { ICharacter } from "../../types/types";
 import "./CharacterCard.scss";
@@ -30,16 +14,6 @@ class CharacterCard extends Component<ICharacter> {
           </div>
         </div>
       </div>
-      //   <div className="character-card">
-      //     <div className="character-name">{name}</div>
-      //     <div className="character-details">
-      //       <ul>
-      //         <li>Gender: {gender}</li>
-      //         <li>Birth year: {birth_year}</li>
-      //         <li>Eye color: {eye_color}</li>
-      //       </ul>
-      //     </div>
-      //   </div>
     );
   }
 }
